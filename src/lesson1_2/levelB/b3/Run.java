021fe4a88484ec276ffc91b29bfeeb2cb9112123
vladb <vladbeloysk@gmail.com>
2025-02-10 20:53:23 +0300

    использование константы из набора JDK

---
+++ src/lesson1_2/levelB/b3/Run.java
@@ -0,0 +1,11 @@
+package lesson1_2.levelB.b3;
+
+/*
+В методе main класса Run написать код, который выводит в консоль значение числа “Пи”. Использовать константу из jdk.
+ */
+public class Run {
+    public static void main(String[] args) {
+        double PI = 3.14159265358979323846;
+        System.out.println(PI);
+    }
+}
