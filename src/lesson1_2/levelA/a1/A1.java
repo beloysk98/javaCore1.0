975e18bb63dbbded577832b38600a1d4cdd08b56
vladb <vladbeloysk@gmail.com>
2025-02-10 19:25:02 +0300

    Обьявление и инициализация переменных примитивного типа. вывод в консоль

---
+++ src/lesson1_2/levelA/a1/A1.java
@@ -0,0 +1,27 @@
+package lesson1_2.levelA.a1;
+
+/*
+Объявление и инициализация переменных.  В методе main класса Run создайте 8 переменных каждого примитивного типа и
+присвойте им любые произвольные значения. Напишите код, который будет выводить значение каждой переменной в консоль.
+ */
+public class A1 {
+    public static void main(String[] args) {
+        byte byteValue = 1;
+        short shortValue = 1;
+        int intValue = 1;
+        long longValue = 1;
+        float floatValue = 1;
+        double doubleValue = 1;
+        char charValue = 'a';
+        boolean booleanValue = true;
+        System.out.println(byteValue);
+        System.out.println(shortValue);
+        System.out.println(intValue);
+        System.out.println(longValue);
+        System.out.println(floatValue);
+        System.out.println(doubleValue);
+        System.out.println(charValue);
+        System.out.println(booleanValue);
+
+    }
+}
