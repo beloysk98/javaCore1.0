651244924686b331977cb5d9ff92ca291add08f4
vladb <vladbeloysk@gmail.com>
2025-02-11 00:48:52 +0300

    создание класса enum с месяцами года и порами года. создание метода который возвращает пору года принимая название месяца

---
+++ src/lesson1_2/levelC/с2/Mounth.java
@@ -0,0 +1,41 @@
+package lesson1_2.levelC.с2;
+
+/*
+Создайте enum класс с перечислением месяцев года. Создайте метод, который на вход принимает значение месяца,
+а на выходе отдаёт пору года. В методе main класса Run вызовите этот метод и выведите в консоль
+результат работы этого метода.
+
+ */
+public enum Mounth {
+    DECEMBER("Декабрь "), JANUARY("Январь"), FEBRUARY("Февраль"), MARCH("Март"), APRIL("Апрель"), MAY("Май"),
+    JUNE("Июнь"), JULY("Июль"), AUGUST("Август"), SEPTEMBER("Сентябрь"), OCTOBER("Октябрь"), NOVEMBER("Ноябрь"),
+    WINTER("Зима"), AUTUMN("Весна"), SUMMER("Лето"), SPRING("Осень");
+    final String info;
+
+    Mounth(String info) {
+        this.info = info;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public static Mounth getSeason(Mounth myEnum) {
+
+        switch (myEnum) {
+            case DECEMBER, JANUARY, FEBRUARY:
+                return Mounth.WINTER;
+
+            case MARCH, APRIL, MAY:
+                return Mounth.AUTUMN;
+
+            case JUNE, JULY, AUGUST:
+                return Mounth.SUMMER;
+
+            case SEPTEMBER, OCTOBER, NOVEMBER:
+                return Mounth.SPRING;
+
+        }
+        return null;
+    }
+}
---
+++ src/lesson1_2/levelC/с2/Run.java
@@ -0,0 +1,9 @@
+package lesson1_2.levelC.с2;
+
+
+public class Run {
+
+    public static void main(String[] args) {
+        System.out.print(Mounth.APRIL.getInfo() + " - это весна  " + Mounth.getSeason(Mounth.APRIL).getInfo() + ".");
+    }
+}
