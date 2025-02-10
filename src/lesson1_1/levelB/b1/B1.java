765e22be96459be7b299f8ad6b6df4d910cc1181
vladb <vladbeloysk@gmail.com>
2025-02-10 17:09:15 +0300

    Изменение в файле .gitignore , создание первого класса

---
+++ src/lesson1_1/levelB/b1/B1.java
@@ -0,0 +1,7 @@
+package lesson1_1.levelB.b1;
+//Написать и запустить “Hello, world!”.
+public class B1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+      }
+}
