b29247950e944d811c6e9752726b9699155d9fe0
vladb <vladbeloysk@gmail.com>
2025-02-10 19:37:33 +0300

    создание константы

---
+++ src/lesson1_2/levelA/a3/Run.java
@@ -0,0 +1,13 @@
+package lesson1_2.levelA.a3;
+
+/*
+В классе Run создайте константу, которая будет хранить в себе номер месяца вашего рождения.
+ Выведите значение константы в консоль.
+ */
+public class Run {
+    private static final byte MOUNTH = 8;
+
+    public static void main(String[] args) {
+        System.out.println(MOUNTH);
+    }
+}
