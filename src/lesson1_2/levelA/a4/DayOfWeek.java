9ee7449fa9ecc508c861950bbecabadc2f331962
vladb <vladbeloysk@gmail.com>
2025-02-10 20:36:23 +0300

    Создание enum класса с переменными дней недели

---
+++ src/lesson1_2/levelA/a4/DayOfWeek.java
@@ -0,0 +1,14 @@
+package lesson1_2.levelA.a4;
+
+/*
+Создайте enum класс с именем DayOfWeek для дней недели.
+ */
+public enum DayOfWeek {
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+}
