4c21291159b7ec821d5ae7122aaa93ea4dbf4b62
vladb <vladbeloysk@gmail.com>
2025-02-11 10:17:13 +0300

    Создание метода для демонстрации работы пост-унарного оператора

---
+++ src/lesson2_1/levelA/a2/Run.java
@@ -0,0 +1,13 @@
+package lesson2_1.levelA.a2;
+
+/*
+Создать переменную типа byte и присвоить ей значение 5. Используя пост-унарный оператор увеличьте значение
+ переменной на единицу и выведите результат в консоль.
+ */
+public class Run {
+    public static void main(String[] args) {
+        byte varByte = 5;
+        varByte++;
+        System.out.println(varByte);
+    }
+}
