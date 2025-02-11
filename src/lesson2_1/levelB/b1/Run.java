8a7180eb4d3e8a6282d50fcf672c82a739182920
vladb <vladbeloysk@gmail.com>
2025-02-11 11:04:38 +0300

    Создание метода для нахождения квадратного корня положительных чисел

---
+++ src/lesson2_1/levelB/b1/Run.java
@@ -0,0 +1,12 @@
+package lesson2_1.levelB.b1;
+
+/*
+В методе main класса Run создайте переменную и присвойте ей произвольное положительное число. Напишите код, который
+будет вычислять значение корня квадратного из переменной и выводить ответ в консоль.
+ */
+public class Run {
+    public static void main(String[] args) {
+        double numberFirst = 5;
+        System.out.println(Math.sqrt(numberFirst));
+    }
+}
