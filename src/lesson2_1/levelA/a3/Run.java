26d87278a3f7f03f7f57d8ae27078cc29f51999b
vladb <vladbeloysk@gmail.com>
2025-02-11 10:21:06 +0300

    Создание метода для демонстрации работы деления по модулю

---
+++ src/lesson2_1/levelA/a3/Run.java
@@ -0,0 +1,13 @@
+package lesson2_1.levelA.a3;
+
+/*
+Создать две переменные типа int и проинициализировать их значениями 7 и 5.
+ Выведите в консоль остаток от деления первого числа на второе.
+ */
+public class Run {
+    public static void main(String[] args) {
+        int varFirst = 7;
+        int varSecond = 5;
+        System.out.println(varFirst % varSecond);
+    }
+}
