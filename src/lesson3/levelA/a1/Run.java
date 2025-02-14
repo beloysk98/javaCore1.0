3c34518043425450723bc707e264773e0469b2fa
vladb <vladbeloysk@gmail.com>
2025-02-14 23:31:07 +0300

    Реализация цикла для вывода цифр от 11 до 10

---
+++ src/lesson3/levelA/a1/Run.java
@@ -0,0 +1,13 @@
+package lesson3.levelA.a1;
+
+/*
+В методе main класса Run используя цикл for выведите в консоль числа от 1 до 10.
+
+ */
+public class Run {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
